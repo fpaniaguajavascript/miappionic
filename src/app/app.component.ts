05aa7b1ab130e51eabfa2c7cc43c6c48a484b108
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-02-14 20:55:48 +0100

    Fin sesión

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Consulta', url: '/formulario', icon: 'videocam' },
+    { title: 'Búsqueda', url: '/formulario', icon: 'videocam' },
     { title: 'Info', url: '/listado', icon: 'alert' },
     { title: 'About', url: '/folder/favorites', icon: 'heart' },
   ];
